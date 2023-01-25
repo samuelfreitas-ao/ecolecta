9ac44d6be7d8eff7c11924e8de568b8e7f6de920
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-25 15:42:02 +0100

    del: remove unused code

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,25 @@
 import React from 'react';
-import { AppLoading } from 'expo';
-import { StyleSheet, StatusBar, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
 import Routes from './src/routes';
 
-export default function App() {
+export default function App () {
   const [fontsLoads] = useFonts({
     Roboto_400Regular
-    ,Roboto_500Medium
-    ,Ubuntu_700Bold
+    , Roboto_500Medium
+    , Ubuntu_700Bold
   });
 
-  if(!fontsLoads){
-    return <AppLoading />
+  if (!fontsLoads) {
+    return
   }
 
   return (
     <>
-    <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-    <Routes/>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Routes />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
