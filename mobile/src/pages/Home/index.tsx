660ba76dc84bbe2b5f8ed17ebb85dc105795255c
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-25 15:57:02 +0100

    style: indetation

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,137 +1,132 @@
 import React, { useState } from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
-import { View, Image, Text, StyleSheet, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, Image, Text, Sele, StyleSheet, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
-const options = [
-  {label: 'Luanda', value:'Luanda'}
-  ,{label: 'Benguela', value:'Benguela'}
-];
-
-const Home = () =>{
+const Home = () => {
   const navigation = useNavigation();
 
   const [province, setProvince] = useState('');
   const [county, setCounty] = useState('');
 
-  function handleNavigateToPoint(){
-    navigation.navigate('Points',{
+  function handleNavigateToPoint () {
+    navigation.navigate('Points', {
       province, county
-    });
+    })
   }
 
-    return (
-      <KeyboardAvoidingView
-        style={{flex:1}}
-        behavior={ Platform.OS === 'ios' ? 'padding': undefined }
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ImageBackground
+        source={require('../../assets/home-background.png')} style={styles.container}
+        imageStyle={{ width: 275, height: 368 }}
       >
-        <ImageBackground 
-            source={require('../../assets/home-background.png')} style={styles.container} 
-            imageStyle={{width:275, height:368}}
-        >
-          <View style={styles.main}>
-              <Image source={ require('../../assets/logo.png')} />
-              <Text style={styles.title}>Seu Marketplace de colecta de resíduos</Text>
-              <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de colectas de forma eficiente</Text>
-          </View>
-          <View style={styles.footer}>
-            <TextInput 
-              style={styles.input}
-              onChangeText={setProvince}
-              placeholder="Digite a província"
-              value={province}
-              autoCorrect={false}
-            />
-            <TextInput 
-              style={styles.input}
-              onChangeText={setCounty}
-              placeholder="Digite o município"
-              value={county}
-              autoCorrect={false}
-            />
-            <RectButton style={styles.button} onPress={handleNavigateToPoint}>
-              <View  style={styles.buttonIcon}>
-                <Text>
-                  <Icon name="arrow-right" color="#FFF" size={24} />
-                </Text>
-              </View>
-              <Text style={styles.buttonText}>
-                Entrar
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <Text style={styles.title}>Seu Marketplace de colecta de resíduos</Text>
+          <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de colectas de forma eficiente</Text>
+        </View>
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={setProvince}
+            placeholder="Digite a província"
+            value={province}
+            autoCorrect={false}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setCounty}
+            placeholder="Digite o município"
+            value={county}
+            autoCorrect={false}
+          />
+          <RectButton style={styles.button} onPress={handleNavigateToPoint}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#FFF" size={24} />
               </Text>
-            </RectButton>
-          </View>
-        </ImageBackground>
-      </KeyboardAvoidingView>
-    );
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
+            </Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
 }
 export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 32,
-    },
-  
-    main: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  
-    title: {
-      color: '#322153',
-      fontSize: 32,
-      fontFamily: 'Ubuntu_700Bold',
-      maxWidth: 260,
-      marginTop: 64,
-    },
-  
-    description: {
-      color: '#6C6C80',
-      fontSize: 16,
-      marginTop: 16,
-      fontFamily: 'Roboto_400Regular',
-      maxWidth: 260,
-      lineHeight: 24,
-    },
-  
-    footer: {},
-  
-    select: {},
-  
-    input: {
-      height: 60,
-      backgroundColor: '#FFF',
-      borderRadius: 10,
-      marginBottom: 8,
-      paddingHorizontal: 24,
-      fontSize: 16,
-    },
-  
-    button: {
-      backgroundColor: '#34CB79',
-      height: 60,
-      flexDirection: 'row',
-      borderRadius: 10,
-      overflow: 'hidden',
-      alignItems: 'center',
-      marginTop: 8,
-    },
-  
-    buttonIcon: {
-      height: 60,
-      width: 60,
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-  
-    buttonText: {
-      flex: 1,
-      justifyContent: 'center',
-      textAlign: 'center',
-      color: '#FFF',
-      fontFamily: 'Roboto_500Medium',
-      fontSize: 16,
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 32,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 10,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  }
+});
