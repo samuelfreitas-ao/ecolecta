367ea9d693032fb5e3402d2fd9cfb6a1b3de2f0c
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-25 15:49:22 +0100

    feat: data validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,24 +1,46 @@
-import express, { request } from 'express';
-import multer from 'multer';
-import multerConfig from './config/multer';
+import express from 'express'
+import { celebrate, Joi } from 'celebrate'
 
-import PointsController from './controller/PointsController';
-import ItemsController from './controller/ItemsController';
+import multer from 'multer'
+import multerConfig from './config/multer'
 
-const routes = express.Router();
-const upload = multer(multerConfig);
+import PointsController from './controller/PointsController'
+import ItemsController from './controller/ItemsController'
 
+const routes = express.Router()
 
+const upload = multer(multerConfig)
 
-const pointsController = new PointsController();
-const itemsController = new ItemsController();
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.post('/items', itemsController.create);
-routes.get('/items', itemsController.index);
-routes.get('/items/:id', itemsController.show);
+routes.post('/items', itemsController.create)
+routes.get('/items', itemsController.index)
+routes.get('/items/:id', itemsController.show)
 
-routes.post('/points', upload.single('image'), pointsController.create);
-routes.get('/points', pointsController.index);
-routes.get('/points/:id', pointsController.show);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        province: Joi.string().required(),
+        county: Joi.string().required(),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  pointsController.create
+)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
-export default routes;
+export default routes
