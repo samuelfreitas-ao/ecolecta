98d29e4f0973347d144a5e273c5c309319ca804b
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-25 15:53:04 +0100

    feat: create-point web page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+// import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Routes as Router, } from "react-router-dom";
 
 import Home from './pages/Home';
 import CreatePoint from './pages/CreatePoint';
 
-const Routes = () =>{
-    //exact valida a igualdade do endereço do browser com a rota e não a ocorrência apenas
-    return (
-        <BrowserRouter>
-        <Route component={Home} path='/' exact />
-            <Route component={CreatePoint} path='/create-point' />
-        </BrowserRouter>
-    );
+const Routes = () => {
+  //exact valida a igualdade do endereço do browser com a rota e não a ocorrência apenas
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route element={<Home />} path='/' />
+        <Route element={<CreatePoint />} path='/create-point' />
+      </Router>
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
